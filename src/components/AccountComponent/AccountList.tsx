904eee9c5c59d3e5cbe9f146e714c175313a3f7e
SonTungIT <son.tung.tn12321gmail.com>
2024-05-30 01:02:10 +0700

    deploy fix

--- src/components/AccountComponent/AccountList.tsx
+++ src/components/AccountComponent/AccountList.tsx
@@ -1,149 +1,38 @@
-import {
-    MRT_GlobalFilterTextField,
-    MRT_TableBodyCellValue,
-    MRT_TablePagination,
-    MRT_ToolbarAlertBanner,
-    flexRender,
-    type MRT_ColumnDef,
-    useMaterialReactTable,
-} from 'material-react-table';
-import {
-    Box,
-    Stack,
-    Table,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableHead,
-    TableRow,
-    Typography,
-} from '@mui/material';
-import { IUser } from '../../models/User';
-// import { type Person, data } from './makeData';
-import data from '../../data/userData';
+import { FaListAlt, FaPlus } from "react-icons/fa";
+import SidebarComponent from "../../components/ManagementComponent/SidebarComponent "
+import { Link } from "react-router-dom";
+import AccountList from "../../components/AccountComponent/AccountList";
 
-const columns: MRT_ColumnDef<IUser>[] = [
-    {
-        accessorKey: 'fullname',
-        header: 'Họ và Tên',
-    },
-    {
-        accessorKey: 'gender',
-        header: 'Giới tính',
-    },
-    {
-        accessorKey: 'email',
-        header: 'Email',
-    },
-    {
-        accessorKey: 'phone',
-        header: 'Số điện thoại',
-    },
-    {
-        accessorKey: 'role',
-        header: 'Chức danh',
-    },
-    {
-        accessorKey: 'birthday',
-        header: 'Sinh nhật',
-        Cell: ({ cell }) => cell.row.original.birthday instanceof Date ? cell.row.original.birthday.toLocaleDateString() : cell.row.original.birthday,
-    },
-    {
-        accessorKey: 'address',
-        header: 'Địa chỉ',
-    },
-    {
-        accessorKey: 'status',
-        header: 'Trạng thái',
-        Cell: ({ cell }) => (
-            <span style={{
-                color: cell.row.original.status ? 'green' : 'red',
-                fontWeight: 'bold',
-                borderRadius: '10px',
-                border: cell.row.original.status ? '1px solid #b7f5a4' : '1px solid #ed9a82',
-                padding: '4px',
-                backgroundColor: cell.row.original.status ? '#b7f5a4' : '#ed9a82',
-            }}>
-                {cell.row.original.status ? 'Active' : 'Inactive'}
-            </span>
-        ),
-    },
-
-];
-
-const AccountList = () => {
-    const table = useMaterialReactTable({
-        columns,
-        data,
-        enableRowSelection: true,
-        initialState: {
-            pagination: { pageSize: 5, pageIndex: 0 },
-            showGlobalFilter: true,
-        },
-        muiPaginationProps: {
-            rowsPerPageOptions: [5, 10, 15],
-            variant: 'outlined',
-        },
-        paginationDisplayMode: 'pages',
-    });
-
+const AccountManagement = () => {
     return (
-        <Stack sx={{ m: '2rem 0' }}>
-            <Typography variant="h4">Danh sách nhân viên</Typography>
-            <Box
-                sx={{
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    alignItems: 'center',
-                }}
-            >
+        <div className="">
+            <div className="grid grid-cols-1 md:grid-cols-12 md:p-0">
+                <div className="md:col-span-2">
+                    <SidebarComponent />
+                </div>
 
-                <MRT_GlobalFilterTextField table={table} />
-                <MRT_TablePagination table={table} />
-            </Box>
-            <TableContainer>
-                <Table>
-                    <TableHead>
-                        {table.getHeaderGroups().map((headerGroup) => (
-                            <TableRow key={headerGroup.id}>
-                                {headerGroup.headers.map((header) => (
-                                    <TableCell align="left" variant="head" key={header.id}>
-                                        {header.isPlaceholder
-                                            ? null
-                                            : flexRender(
-                                                header.column.columnDef.Header ??
-                                                header.column.columnDef.header,
-                                                header.getContext(),
-                                            )}
-                                    </TableCell>
-                                ))}
-                            </TableRow>
-                        ))}
-                    </TableHead>
-                    <TableBody>
-                        {table.getRowModel().rows.map((row, rowIndex) => (
-                            <TableRow
-                                key={row.id}
-                                selected={row.getIsSelected()}
-                                style={{ backgroundColor: rowIndex % 2 === 0 ? 'white' : '#d9d9d9' }}
-                            >
-                                {row.getVisibleCells().map((cell, _columnIndex) => (
-                                    <TableCell align="left" variant="body" key={cell.id}>
-                                        <MRT_TableBodyCellValue
-                                            cell={cell}
-                                            table={table}
-                                            staticRowIndex={rowIndex}
-                                        />
-                                    </TableCell>
-                                ))}
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            <MRT_ToolbarAlertBanner stackAlertBanner table={table} />
-        </Stack>
-    );
-};
+                <div className="md:col-span-10 mt-20">
+                    <div className="flex">
+                        <div className="flex flex-row text-3xl">
+                            <FaListAlt className="text-orange-500" />
+                            <h3 className="font-bold ml-6">Quản lí tài khoản</h3>
+                        </div>
+                        <div className="ml-auto">
+                            <Link to='/admin-register'>
+                                <button className="bg-green-500 hover:bg-organge-500 text-white font-bold p-3 rounded flex items-center justify-between mr-12">
+                                    <FaPlus className="text-base mr-2" />
+                                    <span>Thêm Nhân Viên</span>
+                                </button>
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="w-full overflow-auto">
+                        <AccountList />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
 
-export default AccountList;
+export default AccountManagement;
