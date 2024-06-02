040353c8dad09ff34d85c3df58c2fc68644cdd0a
SonTungIT <son.tung.tn12321@gmail.com>
2024-06-02 22:39:58 +0700

    deploy: CRUD Category, Update status category

--- src/services/features/categorySlice.ts
+++ src/services/features/categorySlice.ts
@@ -3,8 +3,11 @@ import { ICategory, ICategoryCreate } from '../../models/Categoty';
 import axios from 'axios';
 import {
     createCategoryEndpoint,
+    deleteCategoryEndpoint,
     getAllCategoriesEndpoint,
+    updateStatusCategoryEndpoint,
 } from '../api/apiConfig';
+import { toast } from 'react-toastify';
 
 type CategoryState = {
     loading: boolean;
@@ -25,7 +28,7 @@ export const getAllCategories = createAsyncThunk<ICategory[], void>(
     'categories/getAllCategories',
     async (_, thunkAPI) => {
         try {
-            const token = localStorage.getItem('quickServeToken');
+            const token = sessionStorage.getItem('quickServeToken');
             const response = await axios.get(getAllCategoriesEndpoint, {
                 headers: {
                     Authorization: `Bearer ${token}`,
@@ -43,7 +46,7 @@ export const createCategory = createAsyncThunk<ICategoryCreate, Object>(
     'categories/createCategory',
     async (category, thunkAPI) => {
         try {
-            const token = localStorage.getItem('quickServeToken');
+            const token = sessionStorage.getItem('quickServeToken');
             const response = await axios.post(
                 createCategoryEndpoint,
                 category,
@@ -53,11 +56,55 @@ export const createCategory = createAsyncThunk<ICategoryCreate, Object>(
                     },
                 },
             );
+            toast.success('Tạo thể loại thành công ! Có thể sử dụng ngay.');
             return response.data.data;
         } catch (error: any) {
-            return thunkAPI.rejectWithValue(
-                error.response?.data?.errorMessages || 'Unknown error',
+            toast.error('Tạo thể loại thất bại !');
+            return thunkAPI.rejectWithValue(error.response.data);
+        }
+    },
+);
+
+export const updateStatusCategoryById = createAsyncThunk<
+    ICategory,
+    { id: number }
+>('users/updateStatusCategoryById', async ({ id }, thunkAPI) => {
+    try {
+        const token = sessionStorage.getItem('quickServeToken');
+        const response = await axios.put(
+            `${updateStatusCategoryEndpoint}`,
+            { id },
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            },
+        );
+        toast.success('Cập nhật trạng thái thể loại thành công !');
+        return response.data;
+    } catch (error: any) {
+        toast.error('Cập nhật trạng thái thể loại không thành công!');
+        return thunkAPI.rejectWithValue(error.response.data);
+    }
+});
+export const deleteCategoryById = createAsyncThunk<void, { id: number }>(
+    'users/deleteCategoryById',
+    async ({ id }, thunkAPI) => {
+        try {
+            const token = sessionStorage.getItem('quickServeToken');
+            const response = await axios.delete(
+                `${deleteCategoryEndpoint}?Id=${id}`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                },
             );
+            toast.success('Cập nhật trạng thái thể loại thành công !');
+            return response.data;
+        } catch (error: any) {
+            toast.error('Cập nhật trạng thái thể loại không thành công!');
+            return thunkAPI.rejectWithValue(error.response.data);
         }
     },
 );
@@ -91,6 +138,29 @@ export const categorySlice = createSlice({
             state.success = true;
         });
         builder.addCase(createCategory.rejected, (state, action) => {
+            state.loading = false;
+            state.error = action.payload as string[];
+        });
+        builder.addCase(updateStatusCategoryById.pending, (state) => {
+            state.loading = true;
+        });
+        builder.addCase(updateStatusCategoryById.fulfilled, (state) => {
+            state.loading = false;
+            state.success = true;
+        });
+        builder.addCase(updateStatusCategoryById.rejected, (state, action) => {
+            state.loading = false;
+            state.error = action.payload as string[];
+        });
+
+        builder.addCase(deleteCategoryById.pending, (state) => {
+            state.loading = true;
+        });
+        builder.addCase(deleteCategoryById.fulfilled, (state) => {
+            state.loading = false;
+            state.success = true;
+        });
+        builder.addCase(deleteCategoryById.rejected, (state, action) => {
             state.loading = false;
             state.error = action.payload as string[];
         });
