353b7088fab0b79f3a0eff7886843374932084ae
SonTungIT <son.tung.tn12321@gmail.com>
2024-06-08 00:29:58 +0700

    deploy: refactor auth controller and category controller

--- src/services/api/apiConfig.ts
+++ src/services/api/apiConfig.ts
@@ -1,19 +1,20 @@
 export const BASE_URL = 'https://quick-serve-server.azurewebsites.net/api/v1';
 
 //Athentication endpoints
-export const loginEndpoint = `${BASE_URL}/Account/Authenticate`;
-export const registerAccountByAdminEndpoint = `${BASE_URL}/Account/CreateAccount`;
+export const loginEndpoint = `${BASE_URL}/Accounts/authenticate`;
+export const registerAccountByAdminEndpoint = `${BASE_URL}/Accounts`;
 
 //User endpoints
-export const getAllUsersEndpoint = `${BASE_URL}/Account/GetPagedListAccountQuery`;
-export const getUserByIdEndpoint = `${BASE_URL}/Account/GetAccountById`;
+export const getAllUsersEndpoint = `${BASE_URL}/Accounts/paged`;
+export const getUserByIdEndpoint = `${BASE_URL}/Account`;
 
 //Category endpoints
-export const getAllCategoriesEndpoint = `${BASE_URL}/Category/GetPagedListCategory`;
-export const createCategoryEndpoint = `${BASE_URL}/Category/CreateCategory`;
-export const updateStatusCategoryEndpoint = `${BASE_URL}/Category/UpdateStatusCategory`;
-export const deleteCategoryEndpoint = `${BASE_URL}/Category/DeleteCategory`;
-export const renameCategoryEndpoint = `${BASE_URL}/Category/UpdateCategory`;
+export const getAllCategoriesEndpoint = `${BASE_URL}/Categories/paged`;
+export const createCategoryEndpoint = `${BASE_URL}/Categories`;
+export const updateStatusCategoryEndpoint = `${BASE_URL}/Categories/status`;
+export const deleteCategoryEndpoint = `${BASE_URL}//Categories`;
+export const renameCategoryEndpoint = `${BASE_URL}/Categories`;
+
 
 //INGREDIENT endpoints
 export const getAllIngredientEndpoint = `${BASE_URL}/Ingredient/GetPagedListIngredient`;
